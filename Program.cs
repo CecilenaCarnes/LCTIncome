1d09a2e65fd0ddf176760bf3eb367d42bd7f11f0
CecilenaCarnes <cecilenacarnes@gmail.com>
2022-03-09 19:47:01 -0500

    Added class and object but does not work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Luke Carnes Trucking, LLC" );
+//can I calculate the date to put in the write line//
+
+//enter employees - one owner O - others are employees E
+//distinguish between two//
+//owner gets different calculations than employees//
+//Class would be People
+//Object would be owner and employees
+
+
+class People
+{
+    string name1 = "Luke";
+    string name2 = "David";
+   private static void Main(string[] args)
+    {
+        People myObj1 = new People();
+        People myObj2 = new People();
+        Console.WriteLine(myObj1.name1);
+        Console.WriteLine(myObj2.name2);
+    }
+    }
