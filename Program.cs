36b65928c89b9c47368e71d6171b5d678e51de4b
CecilenaCarnes <cecilenacarnes@gmail.com>
2022-03-12 14:04:23 -0500

    Added the date

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Luke Carnes Trucking, LLC" );
 //can I calculate the date to put in the write line//
+DateTime date = DateTime.Now;
+Console.WriteLine(date.ToString("D"));
 
 //enter employees - one owner O - others are employees E
 //distinguish between two//
@@ -11,15 +13,15 @@ Console.WriteLine("Luke Carnes Trucking, LLC" );
 //Object would be owner and employees
 
 
-class People
-{
-    string name1 = "Luke";
-    string name2 = "David";
-   private static void Main(string[] args)
-    {
-        People myObj1 = new People();
-        People myObj2 = new People();
-        Console.WriteLine(myObj1.name1);
-        Console.WriteLine(myObj2.name2);
-    }
-    }
+//class People
+//{
+  //  string name1 = "Luke";
+    //string name2 = "David";
+   //private static void Main(string[] args)
+    //{
+    //    People myObj1 = new People();
+      //  People myObj2 = new People();
+        //Console.WriteLine(myObj1.name1);
+       // Console.WriteLine(myObj2.name2);
+    //}
+    //}
